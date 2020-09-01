1618d0317039474b64e086ad26dec62ac5937333
munch <munch1182@qq.com>
2020-09-01 19:12:41 +0800

    增加了测试用文件和地址

--- app/src/main/java/com/munch/project/one/MainActivity.java
+++ app/src/main/java/com/munch/project/one/MainActivity.java
@@ -2,6 +2,9 @@ package com.munch.project.one;
 
 import android.os.Bundle;
 
+import com.munch.lib.imageload.ImageLoadHelper;
+import com.munch.lib.imageload.glide.GlideStrategy;
+import com.munch.lib.libnative.helper.BarHelper;
 import com.munch.project.one.base.BaseActivity;
 
 
@@ -14,5 +17,9 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BarHelper.with(this).setTransparent();
+        ImageLoadHelper.res(getString(R.string.test_png_3))
+                .strategy(new GlideStrategy())
+                .into(findViewById(R.id.img));
     }
 }
