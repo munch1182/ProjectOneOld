fc5fc54815fcd58ce7856ac77cd7b1d3ed0e8f50
munch <munch1182@qq.com>
2020-09-02 07:14:49 +0800

    Initial commit

---
+++ app/src/main/java/com/munch/test/base/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.munch.test.base
+
+import android.widget.Toast
+import com.munch.lib.libnative.root.RootActivity
+
+/**
+ * Created by Munch on 2019/8/24 11:04
+ */
+abstract class BaseActivity : RootActivity() {
+
+    override fun toast(message: String) {
+        super.toast(message)
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
