2d324590f74730be4cc53c1e56d7950ae0b0d82d
munch <munch1182@qq.com>
2020-09-09 01:13:30 +0800

    FlowLayout2

--- app/src/main/java/com/munch/test/view/TestView2Activity.kt
+++ app/src/main/java/com/munch/test/view/TestView2Activity.kt
@@ -1,8 +1,10 @@
 package com.munch.test.view
 
 import android.os.Bundle
+import android.view.View
 import com.munch.test.R
 import com.munch.test.base.BaseActivity
+import com.munch.test.view.weight.FlowLayout
 import kotlinx.android.synthetic.main.activity_test_view2.*
 
 
@@ -16,5 +18,24 @@ class TestView2Activity : BaseActivity() {
         setContentView(R.layout.activity_test_view2)
         setToolBar(view_tb, "View")
 
+
+        val listener = View.OnClickListener {
+            val gravity = when (it.tag.toString().toInt()) {
+                0 -> FlowLayout.START
+                1 -> FlowLayout.END
+                2 -> FlowLayout.CENTER_HORIZONTAL
+                3 -> FlowLayout.CENTER_VERTICAL
+                4 -> FlowLayout.CENTER
+                5 -> FlowLayout.END_CENTER_VERTICAL
+                else -> FlowLayout.START
+            }
+            view_flow.setGravity(gravity)
+        }
+        view_b1.setOnClickListener(listener)
+        view_b2.setOnClickListener(listener)
+        view_b3.setOnClickListener(listener)
+        view_b4.setOnClickListener(listener)
+        view_b5.setOnClickListener(listener)
+        view_b6.setOnClickListener(listener)
     }
 }
--- app/src/main/java/com/munch/test/view/weight/FlowLayout.kt
+++ app/src/main/java/com/munch/test/view/weight/FlowLayout.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.IntDef
 
 /**
  * 没有写宽高间距因为可以用内部View的margin和padding实现
@@ -12,10 +13,10 @@ import android.view.ViewGroup
 class FlowLayout(context: Context, attrs: AttributeSet?, styleDef: Int) :
     ViewGroup(context, attrs, styleDef) {
 
-    private val rectArray = ArrayList<Int>()
-
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context) : this(context, null)
+
+    private var helper = LayoutHelper()
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         /*super.onMeasure(widthMeasureSpec, heightMeasureSpec)*/
@@ -23,8 +24,8 @@ class FlowLayout(context: Context, attrs: AttributeSet?, styleDef: Int) :
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        helper.reset()
 
-        rectArray.clear()
         var child: View?
         //可用宽度
         val rowsMaxWidth = widthSize - paddingLeft - paddingRight
@@ -33,7 +34,7 @@ class FlowLayout(context: Context, attrs: AttributeSet?, styleDef: Int) :
         //已有高度
         var columnHeight = paddingTop
         var lastRowHeight = paddingTop
-        var isWrap = false
+        var leftWidth: Int
         for (i in 0..childCount) {
             child = getChildAt(i) ?: break
             if (child.visibility == View.GONE) {
@@ -48,28 +49,45 @@ class FlowLayout(context: Context, attrs: AttributeSet?, styleDef: Int) :
             childWidth += params.leftMargin + params.rightMargin
             childHeight += params.topMargin + params.bottomMargin
 
+            if (i == 0) {
+                helper.lineNum = 0
+                helper.lineViewCount = 1
+            }
+            leftWidth = rowsMaxWidth - rowsUsedWidth
             //不换行
-            if (rowsMaxWidth - rowsUsedWidth >= childWidth) {
+            if (leftWidth >= childWidth) {
                 rowsUsedWidth += childWidth
                 //某些单个控件更高则更新高度
                 if (lastRowHeight + childHeight > columnHeight) {
                     columnHeight = lastRowHeight + childHeight
                 }
+                if (i != 0) {
+                    helper.lineViewCount++
+                }
                 //换行
             } else {
+                helper.lineCenterY = lastRowHeight + (columnHeight - lastRowHeight) / 2
+                helper.spaceLeft = leftWidth
+                helper.updateLines()
+                //下一行
                 rowsUsedWidth = paddingLeft + childWidth
                 lastRowHeight = columnHeight
                 columnHeight += childHeight
-                isWrap = true
+
+                helper.lineNum++
+                helper.lineViewCount = 1
             }
 
-            rectArray.add(rowsUsedWidth - childWidth + params.leftMargin)
-            rectArray.add(lastRowHeight + params.topMargin)
-            rectArray.add(rowsUsedWidth - params.rightMargin)
-            rectArray.add(lastRowHeight + childHeight - params.bottomMargin)
+            helper.l = rowsUsedWidth - childWidth + params.leftMargin
+            helper.t = lastRowHeight + params.topMargin
+            helper.r = rowsUsedWidth - params.rightMargin
+            helper.b = lastRowHeight + childHeight - params.bottomMargin
+            helper.updateRect()
         }
+        //是否只有一行
+        val oneLine = helper.lineArray.size == 0
 
-        var width = if (isWrap) widthSize else rowsUsedWidth
+        var width = if (!oneLine) widthSize else rowsUsedWidth
         var height = columnHeight + paddingBottom
 
         if (heightMode == MeasureSpec.EXACTLY) {
@@ -79,21 +97,159 @@ class FlowLayout(context: Context, attrs: AttributeSet?, styleDef: Int) :
             width = widthSize
         }
 
+        if (oneLine) {
+            helper.lineCenterY = heightSize / 2
+            helper.spaceLeft = 0
+            helper.updateLines()
+            //补足最后一行的数据
+        } else {
+            helper.lineCenterY = lastRowHeight + (columnHeight - lastRowHeight) / 2
+            helper.spaceLeft = rowsMaxWidth - rowsUsedWidth
+            helper.updateLines()
+        }
+
         setMeasuredDimension(width, height)
     }
 
     override fun onLayout(change: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        helper.startLayout(0)
+        var lineCount = helper.lineViewCount
+        var lineIndex = 0
         for (i in 0..childCount) {
-            getChildAt(i)?.layout(
-                rectArray[i * 4],
-                rectArray[i * 4 + 1],
-                rectArray[i * 4 + 2],
-                rectArray[i * 4 + 3]
-            )
+            //获取下一行数据
+            if (i >= lineCount) {
+                helper.startLayout(helper.lineNum + 1)
+                lineCount += helper.lineViewCount
+                lineIndex = 0
+            }
+            helper.layout(getChildAt(i), i, lineIndex)
+            lineIndex++
         }
     }
 
     override fun generateLayoutParams(attrs: AttributeSet?): LayoutParams {
         return MarginLayoutParams(context, attrs)
+    }
+
+    fun setGravity(@Gravity gravity: Int) {
+        helper.gravity = gravity
+        requestLayout()
+    }
+
+    @IntDef(START, END, CENTER, CENTER_VERTICAL, CENTER_HORIZONTAL)
+    @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class Gravity
+
+    private class LayoutHelper {
+        var lineNum = 0
+        var lineViewCount = 0
+        var lineCenterY = 0
+
+        //剩下的空间
+        var spaceLeft = 0
+
+        @Gravity
+        var gravity = START
+
+        var rectArray = ArrayList<Int>()
+        var lineArray = ArrayList<Int>()
+
+        var l = 0
+        var t = 0
+        var r = 0
+        var b = 0
+
+        fun reset() {
+            rectArray.clear()
+            lineArray.clear()
+            lineNum = 0
+            lineViewCount = 0
+            lineCenterY = 0
+            spaceLeft = 0
+        }
+
+        fun updateLines() {
+            lineArray.add(lineNum)
+            lineArray.add(lineViewCount)
+            lineArray.add(lineCenterY)
+            lineArray.add(spaceLeft)
+        }
+
+        fun updateRect() {
+            rectArray.add(l)
+            rectArray.add(t)
+            rectArray.add(r)
+            rectArray.add(b)
+        }
+
+        fun layout(view: View?, index: Int, lineIndex: Int) {
+            view ?: return
+            val l = rectArray[index * 4]
+            val t = rectArray[index * 4 + 1]
+            val r = rectArray[index * 4 + 2]
+            val b = rectArray[index * 4 + 3]
+            when (gravity) {
+                START -> view.layout(l, t, r, b)
+                END -> view.layout(l + spaceLeft, t, r + spaceLeft, b)
+                CENTER_HORIZONTAL -> {
+                    //给末尾留下空隙,所以+1
+                    val left = spaceLeft / (lineViewCount + 1)
+                    view.layout(l + left * (lineIndex + 1), t, r + left * (lineIndex + 1), b)
+                }
+                CENTER_VERTICAL -> {
+                    view.layout(
+                        l,
+                        lineCenterY - view.measuredHeight / 2,
+                        r,
+                        lineCenterY + view.measuredHeight / 2
+                    )
+                }
+                CENTER -> {
+                    val left = spaceLeft / (lineViewCount + 1)
+                    view.layout(
+                        l + left * (lineIndex + 1),
+                        lineCenterY - view.measuredHeight / 2,
+                        r + left * (lineIndex + 1),
+                        lineCenterY + view.measuredHeight / 2
+                    )
+                }
+                END_CENTER_VERTICAL -> {
+                    view.layout(
+                        l + spaceLeft,
+                        lineCenterY - view.measuredHeight / 2,
+                        r + spaceLeft,
+                        lineCenterY + view.measuredHeight / 2
+                    )
+                }
+            }
+        }
+
+        fun startLayout(index: Int) {
+            if (index * 4 >= lineArray.size) {
+                return
+            }
+            lineNum = lineArray[index * 4]
+            lineViewCount = lineArray[index * 4 + 1]
+            lineCenterY = lineArray[index * 4 + 2]
+            spaceLeft = lineArray[index * 4 + 3]
+
+            if (spaceLeft < 0) {
+                spaceLeft = 0
+            }
+        }
+
+        override fun toString(): String {
+            return "lineNum:$lineNum,lineViewCount:$lineViewCount,lineCenterY:$lineCenterY,spaceLeft:$spaceLeft,gravity:$gravity"
+        }
+    }
+
+    companion object {
+        const val START = 0
+        const val END = 1
+        const val CENTER = 2
+        const val CENTER_VERTICAL = 3
+        const val CENTER_HORIZONTAL = 4
+        const val END_CENTER_VERTICAL = 5
     }
 }
