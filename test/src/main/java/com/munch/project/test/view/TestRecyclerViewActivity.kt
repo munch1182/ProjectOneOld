47831ce43e690b5e51edc20cf6bcbae873186320
munch1182 <munch1182@qq.com>
2020-12-25 17:11:31 +0800

    TestRecyclerViewActivity

--- test/src/main/java/com/munch/project/test/view/TestRecyclerViewActivity.kt
+++ test/src/main/java/com/munch/project/test/view/TestRecyclerViewActivity.kt
@@ -1,8 +1,23 @@
 package com.munch.project.test.view
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.github.promeg.pinyinhelper.Pinyin
+import com.github.promeg.tinypinyin.lexicons.android.cncity.CnCityDict
 import com.munch.lib.test.TestBaseTopActivity
 import com.munch.project.test.R
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.thread
+import kotlin.random.Random
 
 /**
  * Create by munch1182 on 2020/12/24 17:24.
@@ -10,11 +25,192 @@ import com.munch.project.test.R
 class TestRecyclerViewActivity : TestBaseTopActivity() {
 
     private val letterNavigation: LetterNavigationBarView by lazy { findViewById(R.id.view_letter_navigation) }
+    private val rv: RecyclerView by lazy { findViewById(R.id.view_letter_rv) }
+
+    companion object {
+        private const val COUNT = 5
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_recycler_view)
         letterNavigation.setAllLetters()
         letterNavigation.select("A")
+
+        Pinyin.init(Pinyin.newConfig().with(CnCityDict.getInstance(applicationContext)))
+
+        rv.layoutManager = StaggeredGridLayoutManager(COUNT, StaggeredGridLayoutManager.VERTICAL)
+        rv.adapter = IconAdapter(getItems(), rv)
+    }
+
+    private fun getItems(): ArrayList<IconContentBean> {
+        val arrayList = ArrayList<IconContentBean>()
+        for (i in 0..100) {
+            arrayList.add(IconContentBean.newInstance())
+        }
+        return arrayList
+    }
+
+    data class IconBean(val type: Int, val item: Any) {
+        companion object {
+            const val TYPE_TITLE = 0
+            const val TYPE_CONTENT = 1
+
+            fun newTitle(bean: IconTitleBean) = IconBean(TYPE_TITLE, bean)
+            fun newContent(bean: IconContentBean) = IconBean(TYPE_CONTENT, bean)
+        }
+    }
+
+    data class IconTitleBean(val letter: Char)
+    data class IconContentBean(val name: String, val icon: Any?, var letter: String?) {
+
+        companion object {
+            private val chars = " 如果c为汉字则返回大写拼音如果c不是汉字则返回StringvalueOf(c)".split("")
+
+            fun newInstance(): IconContentBean {
+                return IconContentBean(
+                    chars[Random.nextInt(27)].plus(Random.nextInt(1234)),
+                    null,
+                    null
+                )
+            }
+        }
+
+    }
+
+    private class IconViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private class IconAdapter(
+        private val items: ArrayList<IconContentBean>?,
+        private val rv: View
+    ) :
+        RecyclerView.Adapter<IconViewHolder>() {
+
+        init {
+            handleItems()
+        }
+
+        companion object {
+
+        }
+
+        private val beans: ArrayList<IconBean> = arrayListOf()
+
+        private fun handleItems() {
+            thread {
+                val beansThread: ArrayList<IconBean> = arrayListOf()
+                items ?: return@thread
+                items.forEach {
+                    var toPinyin =
+                        Pinyin.toPinyin(it.name.toCharArray(0, 1)[0]).toLowerCase(Locale.ROOT)
+                    if (toPinyin.isEmpty()) {
+                        toPinyin = "#"
+                    }
+                    try {
+                        toPinyin.toDouble()
+                        toPinyin = "#"
+                    } catch (e: Exception) {
+                        //数字放到#中
+                    }
+                    it.letter = toPinyin
+                }
+                Collections.sort(items, object : Comparator<IconContentBean> {
+                    override fun compare(o1: IconContentBean?, o2: IconContentBean?): Int {
+                        if (o1 == null && o2 == null) {
+                            return 0
+                        }
+                        o1 ?: return 1
+                        o2 ?: return -1
+                        o1.letter?.takeIf { it != "#" } ?: return 1
+                        o2.letter?.takeIf { it != "#" } ?: return -1
+                        return o1.letter!!.compareTo(o2.letter!!)
+                    }
+                })
+                var letter = '?'
+                var index = 0
+                items.forEach {
+                    val i = index % COUNT
+                    val c = it.letter!!.toCharArray(0, 1)[0]
+                    //新的标题
+                    if (letter != c) {
+                        letter = c
+                        if (i != 0) {
+                            for (more in i until COUNT) {
+                                //占位
+                                beansThread.add(
+                                    IconBean.newContent(IconContentBean("", null, null))
+                                )
+                                index++
+                            }
+                        }
+                        beansThread.add(IconBean.newTitle(IconTitleBean(letter)))
+                    } else {
+                        //占位
+                        if (i == 0) {
+                            beansThread.add(IconBean.newTitle(IconTitleBean('?')))
+                            index++
+                        }
+                        beansThread.add(IconBean.newContent(it))
+                    }
+                    index++
+                }
+                beans.clear()
+                beans.addAll(beansThread)
+                rv.post {
+                    notifyDataSetChanged()
+                }
+            }
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return beans[position].type
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IconViewHolder {
+            return when (viewType) {
+                IconBean.TYPE_CONTENT -> IconViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.layout_item_recycler_view_content, parent, false)
+                )
+                else -> IconViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.layout_item_recycler_view_letter, parent, false)
+                )
+            }
+        }
+
+        override fun onBindViewHolder(holder: IconViewHolder, position: Int) {
+            when (holder.itemViewType) {
+                IconBean.TYPE_CONTENT -> {
+                    val item = (beans[position].item as IconContentBean)
+                    holder.itemView.findViewById<TextView>(R.id.item_icon_name).text =
+                        item.name
+                    if (item.name.isEmpty()) {
+                        holder.itemView.findViewById<ImageView>(R.id.item_icon_img).background =
+                            null
+                    } else {
+                        holder.itemView.findViewById<ImageView>(R.id.item_icon_img).apply {
+                            background = ColorDrawable(
+                                ContextCompat.getColor(
+                                    this.context,
+                                    R.color.colorPrimary
+                                )
+                            )
+                        }
+                    }
+                }
+                IconBean.TYPE_TITLE -> {
+                    var toString = (beans[position].item as IconTitleBean).letter.toString()
+                    if ("?" == toString) {
+                        toString = ""
+                    }
+                    holder.itemView.findViewById<TextView>(R.id.item_icon_letter).text =
+                        toString
+                }
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return items?.size ?: 0
+        }
     }
 }
