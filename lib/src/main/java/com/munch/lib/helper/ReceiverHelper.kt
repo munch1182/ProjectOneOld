323dd9cd8b82b73b055aff9023d4477dc75064a0
munch1182 <munch1182@qq.com>
2020-12-28 17:25:56 +0800

    ReceiverHelper

---
+++ lib/src/main/java/com/munch/lib/helper/ReceiverHelper.kt
@@ -0,0 +1,94 @@
+package com.munch.lib.helper
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+
+/**
+ * Create by munch1182 on 2020/12/28 16:48.
+ */
+abstract class ReceiverHelper<T> constructor(
+    private val context: Context,
+    private val actions: Array<String>
+) : AddRemoveSetHelper<T>() {
+
+    private var receiver: ScreenReceiver? = null
+
+    open fun register() {
+        receiver = ScreenReceiver()
+        context.registerReceiver(receiver, IntentFilter().apply {
+            actions.forEach { addAction(it) }
+        })
+    }
+
+    fun setAndRegister(owner: LifecycleOwner, t: T, onDestroy: (() -> Unit)?) {
+        register()
+        super.set(owner, t, onDestroy)
+        unregister(owner)
+    }
+
+    fun setAndRegisterWhenCreate(
+        owner: LifecycleOwner,
+        t: T,
+        onCreate: (() -> Unit)?,
+        onDestroy: (() -> Unit)?
+    ) {
+        registerWhenCreate(owner)
+        super.setWhenCreate(owner, t, onCreate, onDestroy)
+    }
+
+    open fun registerWhenCreate(owner: LifecycleOwner) {
+        owner.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                register()
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                unregister()
+                owner.lifecycle.removeObserver(this)
+            }
+        })
+    }
+
+    private fun unregister(owner: LifecycleOwner) {
+        owner.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                unregister()
+                owner.lifecycle.removeObserver(this)
+            }
+        })
+    }
+
+    open fun unregister() {
+        receiver ?: return
+        context.unregisterReceiver(receiver)
+        receiver = null
+    }
+
+    inner class ScreenReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent ?: return
+            if (arrays.isEmpty()) {
+                return
+            }
+            actions.forEach { action ->
+                if (action == intent.action) {
+                    arrays.forEach {
+                        handleAction(action, context, intent, it)
+                    }
+                    return
+                }
+            }
+        }
+    }
+
+    abstract fun handleAction(action: String, context: Context?, intent: Intent, t: T)
+}
--- lib/src/main/java/com/munch/lib/helper/ScreenReceiverHelper.kt
+++ lib/src/main/java/com/munch/lib/helper/ScreenReceiverHelper.kt
@@ -1,35 +1,16 @@
 package com.munch.lib.helper
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 
 /**
  * Create by munch1182 on 2020/12/14 11:08.
  */
-class ScreenReceiverHelper constructor(private val context: Context) :
-    AddRemoveSetHelper<ScreenReceiverHelper.ScreenStateListener>() {
-
-    private var receiver: ScreenReceiver? = null
-
-    fun register() {
-        if (receiver != null) {
-            return
-        }
-        receiver = ScreenReceiver()
-        context.registerReceiver(receiver, IntentFilter().apply {
-            addAction(Intent.ACTION_SCREEN_ON)
-            addAction(Intent.ACTION_SCREEN_OFF)
-            addAction(Intent.ACTION_USER_PRESENT)
-        })
-    }
-
-    fun unregister() {
-        context.unregisterReceiver(receiver)
-        receiver = null
-        arrays.clear()
-    }
+class ScreenReceiverHelper constructor(context: Context) :
+    ReceiverHelper<ScreenReceiverHelper.ScreenStateListener>(
+        context,
+        arrayOf(Intent.ACTION_SCREEN_ON, Intent.ACTION_SCREEN_OFF, Intent.ACTION_USER_PRESENT)
+    ) {
 
     interface ScreenStateListener {
         fun onScreenOn(context: Context?)
@@ -37,30 +18,21 @@ class ScreenReceiverHelper constructor(private val context: Context) :
         fun onUserPresent(context: Context?)
     }
 
-    inner class ScreenReceiver : BroadcastReceiver() {
-
-        override fun onReceive(context: Context?, intent: Intent?) {
-            intent ?: return
-            if (arrays.isEmpty()) {
-                return
+    override fun handleAction(
+        action: String,
+        context: Context?,
+        intent: Intent,
+        t: ScreenStateListener
+    ) {
+        when (action) {
+            Intent.ACTION_SCREEN_ON -> {
+                t.onScreenOn(context)
             }
-            when {
-                Intent.ACTION_SCREEN_ON == intent.action -> {
-                    arrays.forEach {
-                        it.onScreenOn(context)
-                    }
-                }
-                Intent.ACTION_SCREEN_OFF == intent.action -> {
-                    arrays.forEach {
-                        it.onScreenOff(context)
-                    }
-                }
-                //解锁
-                Intent.ACTION_USER_PRESENT == intent.action -> {
-                    arrays.forEach {
-                        it.onUserPresent(context)
-                    }
-                }
+            Intent.ACTION_SCREEN_OFF -> {
+                t.onScreenOff(context)
+            }
+            Intent.ACTION_USER_PRESENT -> {
+                t.onUserPresent(context)
             }
         }
     }
