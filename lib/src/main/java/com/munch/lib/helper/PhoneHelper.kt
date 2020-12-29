21aae15ea4bef771dd63f42fe1a12286cbb6ff91
munch1182 <munch1182@qq.com>
2020-12-29 18:16:17 +0800

    fix

--- lib/src/main/java/com/munch/lib/helper/PhoneHelper.kt
+++ lib/src/main/java/com/munch/lib/helper/PhoneHelper.kt
@@ -5,6 +5,7 @@ import android.content.res.Resources
 import android.graphics.Point
 import android.os.Build
 import android.util.DisplayMetrics
+import android.util.Size
 import android.view.WindowManager
 import com.munch.lib.BaseApp
 
@@ -21,7 +22,7 @@ object PhoneHelper {
      * 分辨率
      */
     @Suppress("DEPRECATION")
-    fun getScreenWidthHeightReal(context: Context = BaseApp.getInstance()): Pair<Int, Int>? {
+    fun getScreenWidthHeightReal(context: Context = BaseApp.getInstance()): Size? {
         val point = Point()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             context.display?.getRealSize(point)
@@ -30,14 +31,14 @@ object PhoneHelper {
                 context.getSystemService(Context.WINDOW_SERVICE) as WindowManager? ?: return null
             manager.defaultDisplay.getRealSize(point)
         }
-        return Pair(point.x, point.y)
+        return Size(point.x, point.y)
     }
 
     /**
-     * 可见区域，一般是高度的分辨率减去navigation高度
+     * 可见区域，一般是高度减去navigation高度
      */
     @Suppress("DEPRECATION")
-    fun getScreenWidthHeight(context: Context = BaseApp.getInstance()): Pair<Int, Int>? {
+    fun getScreenWidthHeight(context: Context = BaseApp.getInstance()): Size? {
         val manager =
             context.getSystemService(Context.WINDOW_SERVICE) as WindowManager? ?: return null
         val width: Int
@@ -52,7 +53,7 @@ object PhoneHelper {
             width = metrics.widthPixels
             height = metrics.heightPixels
         }
-        return Pair(width, height)
+        return Size(width, height)
     }
 
     fun getStatusBarHeight(context: Context = BaseApp.getInstance()): Int? {
