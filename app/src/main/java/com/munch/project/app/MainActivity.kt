2dde939ba61361e11c6cc239f5e2c67df21e1ec8
munch1182 <munch1182@qq.com>
2021-01-06 17:30:29 +0800

    组件化02-新建common和app

---
+++ app/src/main/java/com/munch/project/app/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.munch.project.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
