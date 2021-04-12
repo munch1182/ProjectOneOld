1f0bb794179955a9c36598457768a8bc46c9093e
munch1182 <munch1182@qq.com>
2021-04-12 14:13:25 +0800

    LetterNavigationBarView

--- test/src/main/java/com/munch/test/project/one/recyclerview/GroupRvActivity.kt
+++ test/src/main/java/com/munch/test/project/one/recyclerview/GroupRvActivity.kt
@@ -4,33 +4,47 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.munch.lib.fast.base.BaseBindAdapter
 import com.munch.lib.fast.base.BaseBindViewHolder
-import com.munch.lib.fast.databinding.ActivityBaseRvBinding
 import com.munch.lib.fast.extend.get
 import com.munch.pre.lib.base.rv.DiffItemCallback
+import com.munch.pre.lib.extend.log
 import com.munch.pre.lib.extend.observeOnChanged
 import com.munch.pre.lib.helper.drawTextInYCenter
 import com.munch.test.project.one.R
 import com.munch.test.project.one.base.BaseTopActivity
+import com.munch.test.project.one.databinding.ActivityGroupRvBinding
 import com.munch.test.project.one.databinding.ItemGroupRvBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 /**
  * Create by munch1182 on 2021/4/9 15:32.
  */
 class GroupRvActivity : BaseTopActivity() {
 
-    private val bind by bind<ActivityBaseRvBinding>(R.layout.activity_base_rv)
+    private val bind by bind<ActivityGroupRvBinding>(R.layout.activity_group_rv)
     private val model by get(AppItemViewModel::class.java)
     private lateinit var appAdapter: BaseBindAdapter<AppItemViewModel.AppGroupItem, ItemGroupRvBinding>
     private val spanCount = 4
+    private val manager by lazy { bind.baseTopRv.layoutManager as GridLayoutManager }
 
     private fun querySize() {
         model.span(spanCount).appItemSortByGroup.observeOnChanged(this) { data ->
             appAdapter.set(data.first)
+            val chars: MutableSet<Char> = data.second.keys
+            val letter = runBlocking {
+                withContext(lifecycleScope.coroutineContext + Dispatchers.Default) {
+                    chars.map { it.toString() }.toMutableList().apply { sort() }
+                }
+            }
+            bind.groupLetter.setLetters(letter)
+            selectRange()
             hideSrl()
         }
     }
@@ -39,13 +53,13 @@ class GroupRvActivity : BaseTopActivity() {
         super.onCreate(savedInstanceState)
         bind.lifecycleOwner = this
         appAdapter = object :
-                BaseBindAdapter<AppItemViewModel.AppGroupItem, ItemGroupRvBinding>(R.layout.item_group_rv) {
+            BaseBindAdapter<AppItemViewModel.AppGroupItem, ItemGroupRvBinding>(R.layout.item_group_rv) {
             init {
                 diffUtil = object : DiffItemCallback<AppItemViewModel.AppGroupItem>() {
 
                     override fun areContentsTheSame(
-                            oldItem: AppItemViewModel.AppGroupItem,
-                            newItem: AppItemViewModel.AppGroupItem
+                        oldItem: AppItemViewModel.AppGroupItem,
+                        newItem: AppItemViewModel.AppGroupItem
                     ): Boolean {
                         val old = oldItem.appItem ?: return false
                         val new = newItem.appItem ?: return false
@@ -55,9 +69,9 @@ class GroupRvActivity : BaseTopActivity() {
             }
 
             override fun onBindViewHolder(
-                    holder: BaseBindViewHolder<ItemGroupRvBinding>,
-                    bean: AppItemViewModel.AppGroupItem,
-                    pos: Int
+                holder: BaseBindViewHolder<ItemGroupRvBinding>,
+                bean: AppItemViewModel.AppGroupItem,
+                pos: Int
             ) {
                 holder.bind.app = bean
             }
@@ -79,7 +93,11 @@ class GroupRvActivity : BaseTopActivity() {
                 }
                 private val padding = resources.getDimension(R.dimen.padding_def)
                 private val manager = layoutManager as LinearLayoutManager
-                override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+                override fun onDrawOver(
+                    c: Canvas,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
                     super.onDrawOver(c, parent, state)
                     val firstPos = manager.findFirstVisibleItemPosition()
                     val endPos = manager.findLastVisibleItemPosition()
@@ -90,10 +108,18 @@ class GroupRvActivity : BaseTopActivity() {
                         val bean = appAdapter.get(i) ?: return
                         if (bean.indexInLetter == 0) {
                             val view = parent.getChildAt(i - firstPos) ?: return
-                            c.drawTextInYCenter(bean.char.toString(), padding,
-                                    view.top + padding + 50f / 2f, paint)
+                            c.drawTextInYCenter(
+                                bean.char.toString(), padding,
+                                view.top + padding + 50f / 2f, paint
+                            )
                         }
                     }
+                }
+            })
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    selectRange()
                 }
             })
             adapter = appAdapter
@@ -101,6 +127,20 @@ class GroupRvActivity : BaseTopActivity() {
         bind.baseTopSrl.apply { setOnRefreshListener { postDelayed({ hideSrl() }, 600L) } }
         showSrl()
         querySize()
+    }
+
+    private fun selectRange() {
+        val firstPos = manager.findFirstVisibleItemPosition()
+        val lastPos = manager.findLastVisibleItemPosition()
+        if (firstPos == -1 || lastPos == -1) {
+            return
+        }
+        val firstBean = appAdapter.get(firstPos) ?: return
+        val lastBean = appAdapter.get(lastPos) ?: return
+        bind.groupLetter.selectRange(
+            firstBean.char.toString(),
+            lastBean.char.toString()
+        )
     }
 
     private fun showSrl() {
