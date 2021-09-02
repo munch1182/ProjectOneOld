851a236804d8ab14652e416eab669496fbb24802
munch1182 <munch1182@qq.com>
2021-09-02 11:17:30 +0800

    Net+

--- applib/src/main/java/com/munch/project/one/applib/net/TestNetActivity.kt
+++ applib/src/main/java/com/munch/project/one/applib/net/TestNetActivity.kt
@@ -1,6 +1,8 @@
 package com.munch.project.one.applib.net
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import com.munch.lib.fast.base.BaseBigTextTitleActivity
 import com.munch.lib.fast.base.get
 import com.munch.project.one.applib.R
@@ -13,12 +15,17 @@ class TestNetActivity : BaseBigTextTitleActivity() {
 
 
     private val bind by bind<ActivityNetBinding>(R.layout.activity_net)
-    private val vm by get(TestNetViewModel::class.java)
+    private val vm by get(NetViewModel::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind.apply {
-            vm = this@TestNetActivity.vm
+            notice = vm.notice().toString()
+            netBtnWifi.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+            }
+            netBtnCellular.setOnClickListener { startActivity(Intent(Settings.ACTION_DATA_ROAMING_SETTINGS)) }
         }
+        vm.notice().observe(this) { bind.notice = it }
     }
 }
