3dfa3d7e0abd75f59aa9234decb195ca2f1d4a45
munch1182 <munch1182@qq.com>
2022-07-08 15:45:48 +0800

    update

--- app/src/main/java/com/munch/template/android/MainActivity.kt
+++ app/src/main/java/com/munch/template/android/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.munch.template.android
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.munch.template.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
 
-        vb.text.text = "${BuildConfig.BUILD_TYPE}\n${BuildConfig.APPLICATION_ID}\n${BuildConfig.VERSION_CODE}\n${BuildConfig.VERSION_NAME}\n${BuildConfig.VERSION_DESC}\n${BuildConfig.BUILD_TIME}"
+        val str = "${BuildConfig.BUILD_TYPE}\n" +
+                "${BuildConfig.APPLICATION_ID}\n" +
+                "${BuildConfig.VERSION_CODE}\n" +
+                "${BuildConfig.VERSION_NAME}\n" +
+                "${BuildConfig.VERSION_DESC}\n" +
+                BuildConfig.BUILD_TIME
+        vb.text.text = str
     }
 }
